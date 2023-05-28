246bcc7883e5e1e3955e3c3932d0f4b2892f3b4d
Kaushik Sarmah (kaushik_cs_32) <kaushiksarmah999@gmail.com>
2023-05-28 20:50:02 +0530

    fixed warings in blood bank

--- med-hub-forntend/src/components/blood-bank/PhotoGallery/PhotoGallery.js
+++ med-hub-forntend/src/components/blood-bank/PhotoGallery/PhotoGallery.js
@@ -16,21 +16,21 @@ const PhotoGallery = () => {
       </div><div className="photo-gallery-paragraph">
               <p>Campaign photos of different parts of world and our prestigious voluntary work.</p>
           </div><div className="photo-gallery-photos">
-              <img src="http://integrisok.com/-/media/Blog/21-Jan/OYH_donating-blood.ashx?revision=60c0b51b-e5ab-44e3-bdc2-28f5c5cb5254" alt="Photo 1" />
-              <img src="https://www.mainlinehealth.org/-/media/images/blog/2018/09/donate-blood/donate-blood_featured_article_event.jpg" alt="Photo 2" />
-              <img src="https://www.who.int/images/default-source/wpro/countries/papua-new-guinea/news/20200614-wbdd-png.jpg?sfvrsn=4286a550_2" alt="Photo 3" />
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPhU0GCRWn7kdV0tmjy7RTq7eHL1wZFs6nA&usqp=CAU" alt="Photo 4" />
-              <img src="http://integrisok.com/-/media/Blog/21-Jan/OYH_donating-blood.ashx?revision=60c0b51b-e5ab-44e3-bdc2-28f5c5cb5254" alt="Photo 1" />
-              <img src="https://www.mainlinehealth.org/-/media/images/blog/2018/09/donate-blood/donate-blood_featured_article_event.jpg" alt="Photo 2" />
-              <img src="https://www.who.int/images/default-source/wpro/countries/papua-new-guinea/news/20200614-wbdd-png.jpg?sfvrsn=4286a550_2" alt="Photo 3" />
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPhU0GCRWn7kdV0tmjy7RTq7eHL1wZFs6nA&usqp=CAU" alt="Photo 4" />
-              <img src="http://integrisok.com/-/media/Blog/21-Jan/OYH_donating-blood.ashx?revision=60c0b51b-e5ab-44e3-bdc2-28f5c5cb5254" alt="Photo 1" />
-              <img src="https://www.mainlinehealth.org/-/media/images/blog/2018/09/donate-blood/donate-blood_featured_article_event.jpg" alt="Photo 2" />
-              <img src="https://www.who.int/images/default-source/wpro/countries/papua-new-guinea/news/20200614-wbdd-png.jpg?sfvrsn=4286a550_2" alt="Photo 3" />
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPhU0GCRWn7kdV0tmjy7RTq7eHL1wZFs6nA&usqp=CAU" alt="Photo 4" />
-              <img src="http://integrisok.com/-/media/Blog/21-Jan/OYH_donating-blood.ashx?revision=60c0b51b-e5ab-44e3-bdc2-28f5c5cb5254" alt="Photo 1" />
-              <img src="https://www.mainlinehealth.org/-/media/images/blog/2018/09/donate-blood/donate-blood_featured_article_event.jpg" alt="Photo 2" />
-              <img src="https://www.who.int/images/default-source/wpro/countries/papua-new-guinea/news/20200614-wbdd-png.jpg?sfvrsn=4286a550_2" alt="Photo 3" />
+              <img src="http://integrisok.com/-/media/Blog/21-Jan/OYH_donating-blood.ashx?revision=60c0b51b-e5ab-44e3-bdc2-28f5c5cb5254" alt="" />
+              <img src="https://www.mainlinehealth.org/-/media/images/blog/2018/09/donate-blood/donate-blood_featured_article_event.jpg" alt="" />
+              <img src="https://www.who.int/images/default-source/wpro/countries/papua-new-guinea/news/20200614-wbdd-png.jpg?sfvrsn=4286a550_2" alt="" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPhU0GCRWn7kdV0tmjy7RTq7eHL1wZFs6nA&usqp=CAU" alt="" />
+              <img src="http://integrisok.com/-/media/Blog/21-Jan/OYH_donating-blood.ashx?revision=60c0b51b-e5ab-44e3-bdc2-28f5c5cb5254" alt="" />
+              <img src="https://www.mainlinehealth.org/-/media/images/blog/2018/09/donate-blood/donate-blood_featured_article_event.jpg" alt="" />
+              <img src="https://www.who.int/images/default-source/wpro/countries/papua-new-guinea/news/20200614-wbdd-png.jpg?sfvrsn=4286a550_2" alt="" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPhU0GCRWn7kdV0tmjy7RTq7eHL1wZFs6nA&usqp=CAU" alt="" />
+              <img src="http://integrisok.com/-/media/Blog/21-Jan/OYH_donating-blood.ashx?revision=60c0b51b-e5ab-44e3-bdc2-28f5c5cb5254" alt="" />
+              <img src="https://www.mainlinehealth.org/-/media/images/blog/2018/09/donate-blood/donate-blood_featured_article_event.jpg" alt="" />
+              <img src="https://www.who.int/images/default-source/wpro/countries/papua-new-guinea/news/20200614-wbdd-png.jpg?sfvrsn=4286a550_2" alt="" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPhU0GCRWn7kdV0tmjy7RTq7eHL1wZFs6nA&usqp=CAU" alt="" />
+              <img src="http://integrisok.com/-/media/Blog/21-Jan/OYH_donating-blood.ashx?revision=60c0b51b-e5ab-44e3-bdc2-28f5c5cb5254" alt="" />
+              <img src="https://www.mainlinehealth.org/-/media/images/blog/2018/09/donate-blood/donate-blood_featured_article_event.jpg" alt="" />
+              <img src="https://www.who.int/images/default-source/wpro/countries/papua-new-guinea/news/20200614-wbdd-png.jpg?sfvrsn=4286a550_2" alt="" />
           </div></>
 
   );
