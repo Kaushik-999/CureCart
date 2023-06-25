37c4bb41f66bcb979aa63242347ad47b9b2a43f9
Kaushik Sarmah (kaushik_cs_32) <kaushiksarmah999@gmail.com>
2023-06-25 17:06:06 +0530

    removed bloodbank

--- med-hub-forntend/src/components/blood-bank/bloodBankMain/BloodBank.js
+++ med-hub-forntend/src/components/blood-bank/bloodBankMain/BloodBank.js
@@ -1,34 +1,9 @@
 import React from 'react'
-import BloodBankCarousel from '../bloodBankCarousel/BloodBankCarousel'
-import BloodBankDonationProcess from '../bloodBankDonationProcess/BloodBankDonationProcess'
-import BloodBankRequestAppointment from '../bloodBankRequestAppointment/BloodBankRequestAppointment'
-import BloodDonationJumbotron from "../BloodDonationJumbotron/BloodDonationJumbotron";
-import CardImage from '../CardImage';
-import CardGrid from '../CardGrid';
-import OpinionCarousel from '../OpinionCarousel/OpinionCarousel';
-import ImageWithButton from '../ImageWithButton/ImageWithButton';
-import CampaignJumbotron from '../CampaignJumbotron/CampaignJumbotron';
-import Campaign from '../Campaign/Campaign';
-import PhotoGallery from '../PhotoGallery/PhotoGallery';
-import SponsorsSection from '../SponsorsSection/SponsorsSection';
 import "./BloodBank.css"
 function BloodBank() {
   return (
     <div className='blood-bank-main'>
-        {/* <h3>Blood Bank</h3> */}
-        <BloodBankCarousel/>
-        <BloodDonationJumbotron />
-        <BloodBankDonationProcess />
-        <BloodBankRequestAppointment/>
-        <CardImage/>
         
-        <CardGrid/>
-        <OpinionCarousel />
-        <ImageWithButton />
-        <CampaignJumbotron />
-        <Campaign />
-        <PhotoGallery />
-        <SponsorsSection />
         
 
     </div>
